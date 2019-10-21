ca920701c5d1acf5387e919f92d908d0c9f59702
“dev8ios” <iggdev8@gmail.com>
2019-10-21 15:33:59 +0800

    更新framework-2019-10-17

--- GameCommunitySDK.framework/Headers/GameCommunityEntry.h
+++ GameCommunitySDK.framework/Headers/GameCommunityEntry.h
@@ -84,5 +84,19 @@
 + (GameCommunityEntryResult *)openTopicGameCommunity:(WeGamersSDKParams*)param topicId:(NSString *)sTopicId supportGameCommunity:(void (^)(BOOL bSupport))supportBlock;
 
 
+/**
+ 打开对应帖子社区
+ @param param 参数请参考WeGamersSDKParams定义。
+ @param sSNSId 帖子Id
+ @param supportBlock 返回当前系统环境是否支持游戏社区
+ @return 打开的窗口结果对象：
+ 1）社区主页视图控制器对象；
+ 2）NSError对象。其中，错误码为
+ -1，表示社区主页视图控制器对象创建失败；
+ -2，表示未找开应用主窗口；
+ -3，表示弹出窗口异常；
+ -4,表示参数填写异常 （可能为空）
+ */
++ (GameCommunityEntryResult *)openSNSGameCommunity:(WeGamersSDKParams*)param snsId:(NSString *)sSNSId supportGameCommunity:(void (^)(BOOL bSupport))supportBlock;
 @end
 
--- README.md
+++ README.md
@@ -20,7 +20,7 @@ WGCommunity is available through [CocoaPods](https://cocoapods.org). To install
 it, simply add the following line to your Podfile:
 
 ```ruby
-pod 'WGCommunity'
+pod 'WGCommunity', :git => "https://github.com/dev8ios/WGCommunity.git"
 ```
 
 ## Author
